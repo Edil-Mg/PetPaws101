4818080073ebdf09e7277474ca5a50564fe3af31
Edil-Mg <135086322+Edil-Mg@users.noreply.github.com>
2023-10-09 22:55:19 -0400

    about page functionality added

--- client/src/Components/AboutUs/AboutUs.jsx
+++ client/src/Components/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ import Footer from '../Footer/Footer';
 import Nav from '../NavBar/Nav';
 
 function AboutUs() {
-  return (
+	return (
 		<>
 			<Nav />
 			<div className="row__title col-12">
@@ -16,10 +16,9 @@ function AboutUs() {
 			<HomePage
 				title="Welcome to Our Store"
 				image="https://images.unsplash.com/photo-1606833694770-40a04762ac16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1051&q=80"
-				alt="Image 1"
 				altText="Product Image"
 				productTitle="Our story"
-				productDescription="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.."
+				productDescription="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 			/>
 			<br />
 			<HomePage
@@ -28,6 +27,8 @@ function AboutUs() {
 				altText="Product Image"
 				productTitle="Our story"
 				productDescription="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				productDescription2="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				isReverse={true} // Set this to true for the second row
 			/>
 			<br />
 			<HomePage
@@ -43,4 +44,45 @@ function AboutUs() {
 	);
 }
 
-export default AboutUs
+export default AboutUs;
+
+
+// function AboutUs() {
+//   return (
+// 		<>
+// 			<Nav />
+// 			<div className="row__title col-12">
+// 				<h2>Get involved</h2>
+// 			</div>
+// 			<br />
+// 			<HomePage
+// 				title="Welcome to Our Store"
+// 				image="https://images.unsplash.com/photo-1606833694770-40a04762ac16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1051&q=80"
+// 				alt="Image 1"
+// 				altText="Product Image"
+// 				productTitle="Our story"
+// 				productDescription="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.."
+// 			/>
+// 			<br />
+// 			<HomePage
+// 				title="Welcome to Our Store"
+// 				image="https://images.unsplash.com/photo-1593134257782-e89567b7718a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
+// 				altText="Product Image"
+// 				productTitle="Our story"
+// 				productDescription="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 			/>
+// 			<br />
+// 			<HomePage
+// 				title="Welcome to Our Store"
+// 				image="https://images.unsplash.com/photo-1589441161120-8781d5644435?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1025&q=80"
+// 				altText="Product Image"
+// 				productTitle="Our story"
+// 				productDescription="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// 			/>
+// 			<br />
+// 			<Footer />
+// 		</>
+// 	);
+// }
+
+// export default AboutUs
--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,19 +1,28 @@
 
 import React from "react";
 import "./Homepage.css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function HomePage(props) {
+	const { image, altText, productTitle, productDescription, productDescription2, isReverse } = props;
+
 	return (
-		<div className="homePage">
+		<div className={`homePage ${isReverse ? "reverse" : ""}`}>
 			<div className="container">
 				<div className="row">
-					<div className="col-md-4 p-2 d-flex">
-						<img src={props.image} alt={props.altText} className="img-fluid" />
+					<div
+						className={`col-md-5 p-2 d-flex ${isReverse ? "order-md-2" : ""}`}
+					>
+						<img src={image} alt={altText} className="img-fluid" />
 					</div>
-					<div className="col-md-8 text-black title">
-						<h3>{props.productTitle}</h3>
-						<p>{props.productDescription}</p>
+					<div
+						className={`col-md-5 text-black title ${
+							isReverse ? "order-md-1" : "order-md-2"
+						}`}
+					>
+						<h3>{productTitle}</h3>
+						<p>{productDescription}</p>
+						<p>{productDescription2}</p>
 					</div>
 				</div>
 			</div>
@@ -24,22 +33,18 @@ function HomePage(props) {
 export default HomePage;
 
 
-// import React from "react";
-// import "./Homepage.css";
-
 // function HomePage(props) {
 // 	return (
 // 		<div className="homePage">
 // 			<div className="container">
 // 				<div className="row">
-// 					<div className="image__one col-md-4 p-2 d-flex">
+// 					<div className="col-md-4 p-2 d-flex">
 // 						<img src={props.image} alt={props.altText} className="img-fluid" />
 // 					</div>
-// 					<div className="row2">
+// 					<div className="col-md-6 text-black title">
 // 						<h3>{props.productTitle}</h3>
 // 						<p>{props.productDescription}</p>
 // 					</div>
-					
 // 				</div>
 // 			</div>
 // 		</div>
@@ -47,6 +52,10 @@ export default HomePage;
 // }
 
 // export default HomePage;
+
+
+
+
 
 
 
